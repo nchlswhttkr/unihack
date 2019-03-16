57b70d415a315f9ab9783fcdc54a8d908ba86243
Nicholas Wong <njw31415@gmail.com>
2019-03-17 04:12:20 +1100

    removed stuff

--- backend/uberride.py
+++ backend/uberride.py
@@ -1,54 +1,38 @@
 from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
-import requests
-import webbrowser
-import oauth2
 
 session = Session(server_token="CUofQlGNUbYK3x9FjX0AtlFjEJCak4O59V61YeGs")
 client = UberRidesClient(session)
 
-# get user to log in
-url = "https://login.uber.com/oauth/v2/authorize?response_type=code&client_id=bNuMgUk4XDsvlVbnnLtJwwEitiflHMPG&scope=" \
-      "history+history_lite+places+profile+request_receipt&redirect_uri=http://localhost:3000"
-webbrowser.open(url)
-print()
-
-
-# Get products for a location
 response = client.get_products(37.77, -122.41)
 products = response.json.get('products')
 
-product_id = products[0].get('product_id')
-
-# Get upfront fare and start/end locations
-estimate = client.estimate_ride(
-    product_id=product_id,
-    start_latitude=37.77,
-    start_longitude=-122.41,
-    end_latitude=37.79,
-    end_longitude=-122.41,
+response = client.get_price_estimates(
+    start_latitude=37.770,
+    start_longitude=-122.411,
+    end_latitude=37.791,
+    end_longitude=-122.405,
     seat_count=2
 )
-fare = estimate.json.get('fare')
 
-# Request a ride with upfront fare and start/end locations
-response = client.request_ride(
-    product_id=product_id,
-    start_latitude=37.77,
-    start_longitude=-122.41,
-    end_latitude=37.79,
-    end_longitude=-122.41,
-    seat_count=2,
-    fare_id=fare['fare_id']
+estimate = response.json.get('prices')
+
+from uber_rides.auth import AuthorizationCodeGrant
+auth_flow = AuthorizationCodeGrant(
+    "bNuMgUk4XDsvlVbnnLtJwwEitiflHMPG",
+    "history history_lite places profile request_receipt",
+    "Crhu4OyRxMV0nHoZYT-Mu8R2izwlDaSupxao_QMF",
+    "http://localhost:5000/uber/success"
 )
+auth_url = auth_flow.get_authorization_url()
 
-request = response.json
-request_id = request.get('request_id')
+session = auth_flow.get_session("http://localhost:5000/uber/success?code=JA.VUNmGAAAAAAAEgASAAAABwAIAAwAAAAAAAAAEgAAAAAAAAG8AAAAFAAAAAAADgAQAAQAAAAIAAwAAAAOAAAAkAAAABwAAAAEAAAAEAAAAPxtIcCH81zNpbi_21NRXyxsAAAAyZYw2AIWkRQEoLeDW1fVF-mJIr0-sslqVPLtmA9T5uXoz4n06j3L1siIBdi9UBCEiThzpt6jzWs0Fr_bBx8g1ITkFar7NgwjJ8SaghbB4UTWNo_7UDqyMZ55Na2Ym5lTxaTxBiCFSVIYn0jdDAAAACWpUq-rVsDw9z8iUSQAAABiMGQ4NTgwMy0zOGEwLTQyYjMtODA2ZS03YTRjZjhlMTk2ZWU")
+client = UberRidesClient(session, sandbox_mode=True)
+credentials = session.oauth2credential
 
-# Request ride details using `request_id`
-response = client.get_ride_details(request_id)
-ride = response.json
+response = client.get_user_profile()
+profile = response.json
 
-# Cancel a ride
-response = client.cancel_ride(request_id)
-ride = response.json
+first_name = profile.get('first_name')
+last_name = profile.get('last_name')
+email = profile.get('email')
